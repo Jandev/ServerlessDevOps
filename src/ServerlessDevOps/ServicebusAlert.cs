64a2a34a85192f8b3349bebc6125fda588328c2e
Jan de Vries <jan@jan-v.nl>
2019-05-09 22:15:39 +0200

    Using T4 template

--- src/ServerlessDevOps/ServicebusAlert.cs
+++ src/ServerlessDevOps/ServicebusAlert.cs
@@ -7,28 +7,44 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using ServerlessDevOps.Model;
 using Newtonsoft.Json;
+using ServerlessDevOps.Templates;
 
 namespace ServerlessDevOps
 {
-    public static class ServicebusAlert
-    {
-        [FunctionName("ServicebusAlert")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
-            ILogger log)
-        {
-            log.LogInformation($"Executing {nameof(ServicebusAlert)}.");
+	public static class ServicebusAlert
+	{
+		[FunctionName("ServicebusAlert")]
+		public static async Task<IActionResult> Run(
+			[HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
+			ILogger log)
+		{
+			log.LogInformation($"Executing {nameof(ServicebusAlert)}.");
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+			string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
 
-            log.LogInformation(requestBody);
-            var alert = JsonConvert.DeserializeObject<AzureMonitorCommonAlertSchema>(requestBody);
+			log.LogInformation(requestBody);
+			
+			var messageData = GetMessageData(requestBody);
+			
+			var messageCard = CreateMessageCard(messageData);
 
-            var messageData = new MessageData(alert);
+			log.LogInformation($"Executed {nameof(ServicebusAlert)}.");
 
-            log.LogInformation($"Executed {nameof(ServicebusAlert)}.");
+			return new OkResult();
+		}
 
-            return new OkResult();
-        }
-    }
+		private static MessageData GetMessageData(string requestBody)
+		{
+			var alert = JsonConvert.DeserializeObject<AzureMonitorCommonAlertSchema>(requestBody);
+			var messageData = new MessageData(alert);
+			return messageData;
+		}
+
+		private static string CreateMessageCard(MessageData messageData)
+		{
+			var messageCard = new AzureMonitorAlertCard(messageData);
+			var messageContent = messageCard.TransformText();
+			return messageContent;
+		}
+	}
 }
